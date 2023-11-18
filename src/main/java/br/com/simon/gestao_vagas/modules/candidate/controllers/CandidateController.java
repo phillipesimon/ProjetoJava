22e32871dea8ebb42980f706b0b5996b4dab95e5
phillipesimon <phillipe.simon@gmail.com>
2023-11-18 07:47:08 -0300

    Criando a estrutura do projeto

---
+++ src/main/java/br/com/simon/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,18 @@
+package br.com.simon.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.simon.gestao_vagas.modules.candidate.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+  @PostMapping("/")
+  public void create(@RequestBody CandidateEntity candidateEntity) {
+    System.out.println("Candidato " + candidateEntity.getEmail());
+  }
+}
---
+++ src/test/java/br/com/simon/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simon.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
