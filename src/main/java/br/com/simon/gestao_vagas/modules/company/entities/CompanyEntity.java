0830bd3feab76fc139103aaff866b1fb548abaf8
phillipesimon <phillipe.simon@gmail.com>
2023-11-19 10:54:21 -0300

    Criando entidade job

--- src/main/java/br/com/simon/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -23,19 +23,18 @@ public class CompanyEntity {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  private String name;
-
-  @NotBlank()
-  @Pattern(regexp = "\\S+", message = "O campo username não deve conter espaço!")
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
   private String username;
 
-  @Email(message = "Insira um email válido!")
+  @Email(message = "O campo [email] deve conter um e-mail válido")
   private String email;
 
-  @Length(min = 10, max = 100)
+  @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
   private String password;
 
   private String website;
+  private String name;
   private String description;
 
   @CreationTimestamp
