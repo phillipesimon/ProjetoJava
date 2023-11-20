ff07d433ea50188607441c7c0cad0c9ea4b0777c
phillipesimon <phillipe.simon@gmail.com>
2023-11-20 13:40:02 -0300

    Refatorando JobController

--- src/main/java/br/com/simon/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/company/entities/JobEntity.java
@@ -13,20 +13,25 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "job")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
-
   private String description;
   private String benefits;
 
-  @NotBlank(message = "Esse campo é obrigatório!")
+  @NotBlank(message = "Esse campo é obrigatório")
   private String level;
 
   @ManyToOne()
