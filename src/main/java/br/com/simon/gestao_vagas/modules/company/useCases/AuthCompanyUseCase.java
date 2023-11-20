c135b97743e20ef0b037b701583196c1fb47f6c3
phillipesimon <phillipe.simon@gmail.com>
2023-11-20 10:22:43 -0300

    Finalizando AuthCompanyUseCase

--- src/main/java/br/com/simon/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -3,23 +3,31 @@ package br.com.simon.gestao_vagas.modules.company.useCases;
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.simon.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.simon.gestao_vagas.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
+
+  @Value("${security.token.secret}")
+  private String secretKey;
+
   @Autowired
   private CompanyRepository companyRepository;
 
   @Autowired
   private PasswordEncoder passwordEncoder;
 
-  public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
-    var company = this.companyRepository.findByUsername(authCompanyDTO.getPassword()).orElseThrow(
+  public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
         () -> {
           throw new UsernameNotFoundException("Company not found!");
         });
@@ -28,5 +36,10 @@ public class AuthCompanyUseCase {
     if (!passwordMatches) {
       throw new AuthenticationException();
     }
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var token = JWT.create().withIssuer("javagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+    return token;
   }
 }
