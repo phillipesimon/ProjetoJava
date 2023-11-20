7847376a594840826c7304cb6a36f29bc993d1dc
phillipesimon <phillipe.simon@gmail.com>
2023-11-20 07:35:30 -0300

    Job Controller

--- src/main/java/br/com/simon/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/company/entities/JobEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -21,15 +22,18 @@ public class JobEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String description;
   private String benefits;
+
+  @NotBlank(message = "Esse campo é obrigatório!")
   private String level;
 
   @ManyToOne()
   @JoinColumn(name = "company_id", insertable = false, updatable = false)
   private CompanyEntity companyEntity;
 
-  @Column(name = "company_id")
+  @Column(name = "company_id", nullable = false)
   private UUID companyId;
 
   @CreationTimestamp
