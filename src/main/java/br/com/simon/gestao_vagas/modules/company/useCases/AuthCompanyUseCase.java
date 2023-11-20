e5209566da9d64d7ca234bdd8b3cf8d1d07fd4d4
phillipesimon <phillipe.simon@gmail.com>
2023-11-20 12:29:52 -0300

    Recuperando JWT

--- src/main/java/br/com/simon/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.simon.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +40,7 @@ public class AuthCompanyUseCase {
       throw new AuthenticationException();
     }
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
-    var token = JWT.create().withIssuer("javagas")
+    var token = JWT.create().withIssuer("javagas").withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
         .withSubject(company.getId().toString())
         .sign(algorithm);
     return token;
