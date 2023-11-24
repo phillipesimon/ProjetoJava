2578ec09990c3b5a2f33aa46031224c113a8419e
phillipesimon <phillipe.simon@gmail.com>
2023-11-24 16:36:32 -0300

    Definindo o retorno dos erros

--- src/main/java/br/com/simon/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.simon.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +14,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProfileCandidateResponseDTO {
 
+  @Schema(example = "Desenvolvedora JAVA")
   private String description;
+
+  @Schema(example = "Maria")
   private String username;
+
+  @Schema(example = "maria@gmail.com")
   private String email;
+
   private UUID id;
+
+  @Schema(example = "Maria de Souza")
   private String name;
 
 }
