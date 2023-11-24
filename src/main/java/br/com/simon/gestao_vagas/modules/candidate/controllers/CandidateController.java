0ec76ae8e29697aaf8bcff4d7bd3d8b707874a54
phillipesimon <phillipe.simon@gmail.com>
2023-11-24 11:52:41 -0300

    UseCase listar todas as vagas

--- src/main/java/br/com/simon/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/simon/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.simon.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,15 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.simon.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.simon.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.simon.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.simon.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.simon.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.validation.Valid;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -26,6 +30,9 @@ public class CandidateController {
 
   @Autowired
   private ProfileCandidateUseCase profileCandidateUseCase;
+
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -44,9 +51,16 @@ public class CandidateController {
 
     try {
       var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
+      System.out.println("  PROFILE VINDO " + profile);
       return ResponseEntity.ok().body(profile);
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
+
+  @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
   }
 }
